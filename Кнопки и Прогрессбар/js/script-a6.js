a757e1508626dbb6c66f0c2c4090821306d70b1b
Eggar111111111 <60366309+Eggar111111111@users.noreply.github.com>
2020-02-20 01:59:14 +0300

    Add files via upload

---
+++ Кнопки и Прогрессбар/js/script-a6.js
@@ -0,0 +1,36 @@
+let bar = document.getElementById("progressBar");
+let width = 1;
+
+function increment() {
+
+console.log(bar.style.width = width++ + "%");
+$("#progressBar").attr("width", bar);
+
+}
+
+console.log(bar.style.width)
+$("#btn1").click(increment);
+
+
+//button 2
+function increment() {
+
+console.log(bar.style.width = width++ +"%");
+$("#progressBar").attr("width", bar);
+
+}
+
+console.log(bar.style.width)
+$("#btn2").click(increment);
+
+//button 3
+function increment() {
+
+console.log(bar.style.width = width++ + "%");
+$("#progressBar").attr("width", bar);
+
+}
+
+console.log(bar.style.width)
+$("#btn3").click(increment);
+
